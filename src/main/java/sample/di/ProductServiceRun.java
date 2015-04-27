92f9ef20f3f140a2c114055ef97e1db88f7d2783
alohawave2014 <m.sato.888@gmail.com>
2015-04-27 14:21:08 +0900

    eclipse 上ではuncommitedで表示されるのでcommitしてみる

--- src/main/java/sample/di/ProductServiceRun.java
+++ src/main/java/sample/di/ProductServiceRun.java
@@ -17,23 +17,47 @@ import sample.di.business.service.ProductService;
  */
 public class ProductServiceRun {
 
+    private enum ExecType{
+        byAppConfig,
+        byXml,
+    }
+    
     private static Logger log = LoggerFactory.getLogger(ProductServiceRun.class);    
     
     public static void main(String[] args) {
         log.info("start");
         
         ProductServiceRun productServiceRun = new ProductServiceRun();
-        productServiceRun.execute();
+
+        productServiceRun.execute(ExecType.byAppConfig);
+        //productServiceRun.execute(ExecType.byXml);
+        
         log.info("end");
     }
 
-    private void execute() {
-        // java annotation based の場合
-        //AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(AppConfig.class);
         
-        // appliactionContext.xml　で宣言した場合
-        ApplicationContext ctx = new ClassPathXmlApplicationContext("META-INF/applicationContext.xml");
+    private void execute(ExecType execType) {
         
+        ApplicationContext ctx = null;
+        switch (execType) {
+        case byAppConfig:
+            // java annotation based の場合
+            ctx = new AnnotationConfigApplicationContext(AppConfig.class);
+            MainProc(ctx);
+            ((AnnotationConfigApplicationContext)ctx).close();
+            break;
+        case byXml:
+            // appliactionContext.xml　で宣言した場合
+            ctx = new ClassPathXmlApplicationContext("META-INF/applicationContext.xml");
+            MainProc(ctx);
+            ((ClassPathXmlApplicationContext)ctx).close();
+            break;            
+        default:
+            break;
+        }
+    }
+
+    private void MainProc(ApplicationContext ctx) {
         // inject to property sample
         ProductService producService = ctx.getBean(ProductService.class);
         Product product = producService.findProduct("hoge");
@@ -42,9 +66,6 @@ public class ProductServiceRun {
         // define message in config file sample
         MessageService messageService = ctx.getBean(MessageService.class);
         log.info("MessageService : {}", messageService.getMessage());
-        
-        ((ClassPathXmlApplicationContext)ctx).close();
-        //ctx.close();
     }
 
 }
